7fb38e04fcade5daf9a309a40edaa0eabd344ba9
Kyu Lee <smailq@gmail.com>
2013-02-15 16:34:24 -0500

    Initial version

---
+++ hello_world_api.py
@@ -0,0 +1,42 @@
+from cosmic.api import API
+
+# Define name of the API
+api = API('cosmic_hello_world')
+
+# Set of people
+people = set()
+
+
+@api.action(
+  accepts={"type": "string"},
+  returns={"type": "string"}
+)
+def remember_me(name):
+  """Remember someone upto 20 people.
+  """
+  if name not in people:
+    # Too many people to remember!
+    if len(people) > 10:
+      people.pop()
+    # Add new person
+    people.add(name)
+    return "Hello, %s! I'll remember you." % name
+  else:
+    return "Welcome back, %s." % name
+
+
+# List people
+@api.action(
+  returns={
+    "type": "array",
+    "items": {"type": "string"}
+  }
+)
+def list_people():
+  """List everyone I know.
+  """
+  return list(people)
+
+
+# Start listening to HTTP requests
+api.run(port=8080)
